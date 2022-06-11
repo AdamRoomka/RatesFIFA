e6aa7f08504eedb6bab2ab45918dbbb347714a90
JackDev42 <jacek.romasevski@gmail.com>
2022-06-11 23:59:31 +0300

    match update

--- Back/routes/matchRoutes.js
+++ Back/routes/matchRoutes.js
@@ -3,12 +3,14 @@ const express = require("express");
 const {
   saveMatch,
   getAllMatches,
-  getMatchById
+  getMatchById,
+  updateMatch
 } = require("./../controllers/matchController");
 
 const router = express.Router();
 
 router.route("/matches/").patch(saveMatch)
+router.route("/matches/:id/").patch(updateMatch)
 router.route("/matches/").get(getAllMatches)
 router.route("/matches/:id/").get(getMatchById)
 
