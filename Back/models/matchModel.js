124eb8cc31eb6a66ebd518153f99359f66b6602c
JackDev42 <jacek.romasevski@gmail.com>
2022-06-11 22:56:17 +0300

    match saving

--- Back/models/matchModel.js
+++ Back/models/matchModel.js
@@ -7,10 +7,10 @@ const Date = {
 
 const DBSchema = mongoose.Schema(
   {
-    team1: {type: Schema.Types.ObjectId, ref: 'country'},
-    team2: {type: Schema.Types.ObjectId, ref: 'country'},
-    score1: {defaul:0,type:Number},
-    score2:{default:0,type:Number},
+    team1: {type: mongoose.Schema.Types.ObjectId, ref: 'country'},
+    team2: {type: mongoose.Schema.Types.ObjectId, ref: 'country'},
+    score1: {default:0,type:Number},
+    score2: {default:0,type:Number},
     time : { type : Date }
   },
   { timestamps: true }
--- Back/routes/teamRoutes.js
+++ Back/routes/teamRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const {
   getAllTeams,
+  getTeamById
 } = require("./../controllers/teamController");
 
 const router = express.Router();
 
 router.route("/teams/").get(getAllTeams);
+router.route("/teams/:id/").get(getTeamById);
+
 
 module.exports = router;
