759fd29b243dea74f5bf0c64600fb227028c15b2
JackDev42 <jacek.romasevski@gmail.com>
2022-10-21 15:37:16 +0300

    cors fix

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Bypass-Tunnel-Reminder','AdamPrzegraStawki');
 
   if ('OPTIONS' == req.method) {
     res.send(200);
@@ -105,7 +106,9 @@ express()
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested, Content-Type, Accept Authorization"
+    
   )
+  res.header('Bypass-Tunnel-Reminder','AdamPrzegraStawki')
   if (req.method === "OPTIONS") {
     res.header(
       "Access-Control-Allow-Methods",
