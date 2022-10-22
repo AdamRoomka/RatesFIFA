afd2a97652202d0efafdea6a23e96d7cc6ccc71a
JackDev42 <jacek.romasevski@gmail.com>
2022-10-22 13:04:50 +0300

    test cors

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Bypass-Tunnel-Reminder','AdamPrzegraStawki');
-
+  res.header('Referer', 'https://rates-fifa.loca.lt/')
   if ('OPTIONS' == req.method) {
     res.send(200);
   }
