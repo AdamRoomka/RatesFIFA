edb48668d98e8147350eea18fe129e547d2c088b
JackDev42 <jacek.romasevski@gmail.com>
2022-11-19 00:30:54 +0200

    user email saving

--- models/userModel.js
+++ models/userModel.js
@@ -8,6 +8,7 @@ const Date = {
 const DBSchema = mongoose.Schema(
   {
     name: {type:String},
+    email: {typeString},
     login: {type:String},
     password: {type:String},
     score: {type:Number,default:0},
