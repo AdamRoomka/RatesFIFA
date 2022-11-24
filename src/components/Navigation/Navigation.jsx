9feee214afd76f847cd38ebac340fb4521e374f9
AdamRoomka <adam.romas12@gmail.com>
2022-11-24 21:58:33 +0200

    repaired

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -23,10 +23,62 @@ function Navigation({ role, name }) {
   let user = `${name}`;
   const names = user.split(` `);
 
-
-
   return navigacja ? (
     <>
+      <nav className=" text-warning d-lg-none d-md-none d-sm-flex flex-column flex-wrap">
+        <div class="pos-f-t m-3">
+            <ul class="nav d-flex justify-content-center">
+              <li className="m-3">
+                <h3 className="none me-4">
+                  {names[0]} <CgProfile />
+                </h3>
+              </li>
+              <li className="m-3 fs-2">
+                <Link className="none me-4" to="/">
+                  Główna strona
+                </Link>
+              </li>
+              <li className="m-3 fs-2">
+                <Link className="none me-4" to="/stawki">
+                  Stawki
+                </Link>
+              </li>
+              {role === "admin" ? (
+                <li className="m-3 fs-2">
+                  <Dropdown as={ButtonGroup}>
+                    <Link to="/admin" className="none me-1 fs-2 my-0 pb-2">
+                      Admin
+                    </Link>
+                    <Dropdown.Toggle
+                      split
+                      variant="none"
+                      className="none me-3 pb-2"
+                    />
+                    <Dropdown.Menu>
+                      <Link className="none p-3 fs-2 fs-5" to="/admin/matches">
+                        Mecze
+                      </Link>
+                      {/* <Link className="none p-3 fs-5" to="/admin/history">
+                    History
+                  </Link> */}
+                      <Link className="none p-3 fs-5" to="/admin/users">
+                        Users
+                      </Link>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </li>
+              ) : (
+                ""
+              )}
+              <li className="m-3">
+                <Link to="/" className="none fs-2" onClick={logOut}>
+                  Log Out
+                </Link>
+              </li>
+            </ul>
+        </div>
+      </nav>
+
       <nav className="menu p-4">
         <ul className=" d-lg-flex d-md-flex d-sm-none d-none justify-content-end fs-4">
           <li>
